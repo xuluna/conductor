b87dd5660079c75c274d72225e607ea33877cc77
Luna Xu <xuluna@ibm.com>
2020-03-04 15:33:59 -0500

    debugging

--- conductor/client.py
+++ conductor/client.py
@@ -122,9 +122,11 @@ class Client():
         """Retrieve all the results from the player for the current phase"""
         done = False
         while not done:
+            print ("I'm in conductor")
             sock, addr = self.ressock.accept()
             data = self.len_recv(sock)
             message = pickle.loads(data)
+            print (message)
             if type(message) == retval.RetVal:
                 if message.code == retval.RETVAL_DONE:
                     print ("done")
